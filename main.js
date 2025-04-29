962d7e95f34c2c78c2d1405a42948a9957fd513f
lizzyocm <elizabethoca13@icloud.com>
2025-04-28 21:44:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ document.getElementById('app').innerHTML = `
 
       Quiero desearte unos hermosos 25 años y que en esta etapa de tu vida seas tan feliz como sea posible. Que puedas cumplir todos esos objetivos que te ponés en mente y que todo lo que te toque vivir sea éxito y suerte. Incluso si no es conmigo, o si alguna vez nuestros caminos se separan, nunca voy a dejar de desearte felicidad, y que cada 28 de abril puedas decir que fue otro año valioso, con muchos motivos para agradecer.<br><br>
 
+      <img class="foto-carta" src="https://i.imgur.com/ueL19yt.jpg" alt="foto1"><br><br>
+
       Sos el amor de mi vida. Aunque solo te tuve durante casi dos años, desde la primera vez que hablamos sentí que te conocía de toda la vida. Me permitiste conocer partes tuyas del pasado, cada piedrita que te llevó a lograr todo lo que hoy tenés. Estoy muy orgullosa del hombre en el que te convertiste, y también del hombre que fuiste, de todo lo que afrontaste solo y superaste sin ayuda de nadie, de tu resiliencia y tu gran voluntad.<br><br>
 
+      <img class="foto-carta" src="https://i.imgur.com/jGAijvL.jpg" alt="foto2"><br><br>
+
       Aunque muchas veces me dijiste que fue gracias a mí que encontraste tu camino, siempre te voy a repetir que todas las cosas buenas que te rodean estaban destinadas a ser, porque vos solito pudiste afrontarlas. Nacimos solos y solos nos vamos, pero qué regalo es encontrarse en el medio, porque no hay una sola parte tuya que no admire: admiro tu fuerza de voluntad, admiro tu inteligencia, admiro tu corazón, admiro a las mujeres que te hicieron ser quien sos hoy, admiro tu sabiduría.<br><br>
+
+      <img class="foto-carta" src="https://i.imgur.com/NfvhKil.jpg" alt="foto3"><br><br>
 
       Y aunque no lo demuestre (básicamente porque todo el tiempo me aconsejás como si ya supieras todo y me da alta bronca), sos mi mayor ejemplo a seguir. No lo digo por ser tu novia, sino porque como persona sos una muestra de que ninguna angustia es eterna y que siempre viene algo mejor (yo, jejeje).<br><br>
 
       Sos un gran compañero, y deseo de todo corazón ser siempre la persona que elijas para acompañarte, para que este largo camino de la vida sea un poquito más ameno estando juntos.<br><br>
+
+      <img class="foto-carta" src="https://i.imgur.com/Huv4EjY.jpg" alt="foto4"><br><br>
 
       Gracias por haber nacido y por hacerme la mujer más afortunada. Gracias por siempre cuidarme, por haber tomado lo mejor de tu crianza y transformar lo peor en lo que no quisiste ser. Gracias por compartir un año más este día conmigo y por todo el amor que me demostrás y generás, por cuidar mi corazón y mejorar cada día como hombre para mí.<br><br>
 
